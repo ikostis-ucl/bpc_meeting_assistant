af7576dcf4829bcd082232aa165e820a32ad357a
ikostis <ioannis.kostis@uclouvain.be>
2024-11-19 11:05:08 +0100

    20241119: Licensing.

--- app/engine/inference/base_inference.py
+++ app/engine/inference/base_inference.py
@@ -9,12 +9,9 @@ class BaseInference:
     def __init__(self, args):
         self.model = Groq(model="llama-3.1-70b-versatile", api_key=args.groq_api_key,
                           model_kwargs={"seed": 42}, temperature=0.0)
-
         self.index = load_index(args)
-
         self.embedding_model = HuggingFaceEmbedding(model_name=args.embeddings_model,
                                                     cache_folder=args.embeddings_cache_dir)
-
         self.prompt_template = PromptTemplate(
             "Vous êtes un(e) assistant(e) qui aide un chef de projet à extraire des informations de documents qui "
             "incluent le déroulement de réunions autour d'un certain projet. Il s'agit de la requête du chef "
