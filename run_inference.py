6e600eb9864681bf7faa07fab8649280a84ec86f
ikostis <ioannis.kostis@uclouvain.be>
2025-08-08 12:03:26 +0200

    20250808: RAG times logging with decorators.

--- run_inference.py
+++ run_inference.py
@@ -2,8 +2,8 @@ import os
 import warnings
 
 # from app.scripts.Test import TestRAG as Scenario
-from app.scripts.benchmark_retrieval import BenchmarkRetrieval as Scenario
-# from app.scripts.benchmark_rag import BenchmarkRAG as Scenario
+# from app.scripts.benchmark_retrieval import BenchmarkRetrieval as Scenario
+from app.scripts.benchmark_rag import BenchmarkRAG as Scenario
 
 if __name__ == "__main__":
     warnings.simplefilter(action='ignore', category=FutureWarning)
