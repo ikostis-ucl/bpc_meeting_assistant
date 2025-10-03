279a207a471363c249b3c710d9151ae11154dbb7
ikostis <ioannis.kostis@uclouvain.be>
2025-10-03 09:26:13 +0200

    20251002: Plots.

--- app/utils/benchmark_utils.py
+++ app/utils/benchmark_utils.py
@@ -1,12 +1,12 @@
 BENCHMARK_QUESTIONS_INDEX = {
     "1": "Quelle est la couleur choisie (RAL) pour les châssis ?",
     "2": "Liste des décisions prises concernant le carrelage des salles de bain (SDBs) et les dates (jour/mois/année) auxquelles elles ont été prises.",
-    # "3": "Quelle est la date de remise des espaces communs/publics ?",
+    # "3": "Quelle est la date de remise des espaces communs/publics ?", # Removed as no relevant pages in ground truth
     "4": "Quelles sont les décisions qui ont étés prises pour les acrotères des terrasses ?",
     "5": "Pourrais-je avoir toutes les informations concernant l'ascenseur vélo ?",
     "6": "Pourrais-je avoir un historique concernant les décisions prises pour les couvre-murs ?",
     "7": "Informations concernant les faux-plafonds ?",
-    # "8": "Quelles finitions pour les halls d'entrée ?",
+    # "8": "Quelles finitions pour les halls d'entrée ?", # Removed as no relevant pages in ground truth
     "9": "Quelle isolation a été choisi pour les plafonds du (sous-sol) -1 ?",
     "10": "Pourrais-je avoir une liste des remarques faites par le SECO ?"
 }
